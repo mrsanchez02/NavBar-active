40ca80c17de68e704da24ac796f758b2e019159c
Jose L. Sanchez <leandrosanchez.it@gmail.com>
2022-08-09 18:09:38 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+let ul = document.querySelector('ul');
+let li = document.querySelectorAll('li');
+
+li.forEach(element =>{
+  element.addEventListener('click', () => {
+    ul.querySelector('.active').classList.remove('active');
+    element.classList.add = 'active';
+  })
+})
