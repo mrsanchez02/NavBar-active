bf475620e49d6e814530cbd2489b18a06b17a5a0
Jose Sanchez <53561506+mrsanchez02@users.noreply.github.com>
2022-08-09 18:15:12 -0400

    Update app.js
    Fixed problem when adding the class Active.

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ let li = document.querySelectorAll('li');
 li.forEach(element =>{
   element.addEventListener('click', () => {
     ul.querySelector('.active').classList.remove('active');
-    element.classList.add = 'active';
+    element.classList.add('active');
   })
 })
